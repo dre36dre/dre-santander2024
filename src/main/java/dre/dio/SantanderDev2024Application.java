724ddc52408419f895cb601f5dd5bc21b66309de
dre36dre <dre.son36@gmail.com>
2024-07-26 19:06:31 -0300

    asdfsadf

--- src/main/java/dre/dio/SantanderDev2024Application.java
+++ src/main/java/dre/dio/SantanderDev2024Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SantanderDev2024Application {
 	
 
+	
 public static void main(String[] args) {
 		SpringApplication.run(SantanderDev2024Application.class, args);
 	}
